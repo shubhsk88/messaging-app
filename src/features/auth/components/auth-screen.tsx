306d90e6dfac6b8983f18e994b81d04274cd08d8
Shubham SIngh <shubham.singh@cognite.com>
2024-09-04 05:48:26 +0200

    Adding the sign in card

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -10,7 +10,7 @@ export const AuthScreen = () => {
   return (
     <div className="h-full flex items-center justify-center bg-[#5c3b58]">
       <div className="md:h-auto md:w-[420px]">
-        {flow === 'signIn' ? <SignInCard /> : <SignUpCard />}
+        {flow === 'signIn' ? <SignInCard setState={setFlow} /> : <SignUpCard />}
       </div>
     </div>
   );
