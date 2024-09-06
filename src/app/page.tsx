5e1b60a045f02b26af1b5c139f77550072c1c332
Shubham SIngh <shubham.singh@cognite.com>
2024-09-06 06:12:53 +0200

    Setting up github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import { AuthScreen } from '@/features/auth/components/auth-screen';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 export default function Home() {
-  return <div>hello worls</div>;
+  const { signOut } = useAuthActions();
+  return (
+    <div>
+      hello worls
+      <Button onClick={signOut}>Sign out</Button>
+    </div>
+  );
 }
