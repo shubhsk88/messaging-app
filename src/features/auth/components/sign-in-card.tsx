d5a44f71ed53eaf11f08486ca2baeceb6261edfd
Shubham SIngh <shubham.singh@cognite.com>
2024-09-06 06:26:46 +0200

    Updating the google auth

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -69,7 +69,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
         <div className="flex flex-col gap-y-2.5">
           <Button
             disabled={false}
-            onClick={() => {}}
+            onClick={() => handleProviderSignIn('google')}
             variant="outline"
             size="lg"
             className="w-full relative"
